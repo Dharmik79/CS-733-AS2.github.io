9ddf1007307756c53c0252c1b2dea679b1c6387c
Dharmik7 <dholariyadharmik7@gmail.com>
2022-10-17 00:00:50 -0700

    added the comment in the code

--- trackball.js
+++ trackball.js
@@ -4,14 +4,18 @@ var canvas;
 var gl;
 
 var numPositions = 36;
-
+// Array for storing the positions of points
 var positions = [];
+// Array for storing the color for each point
 var colors = [];
+// Variable to change Color from black to white and vice-versa
 var changeColor;
 var rotationMatrix;
 var rotationMatrixLoc;
 var rotateAxis = false;
+// Initial angle for rotation of the cube
 var angle = 0.0;
+// Initial axis for rotation of the cube
 var axis = vec3(0, 0, 1);
 
 var trackingMouse = false;
@@ -20,7 +24,7 @@ var trackballMove = false;
 var lastPos = [0, 0, 0];
 var curx, cury;
 var startX, startY;
-
+// Check for the mouse to track the cube for rotation
 function trackballView(x, y) {
   var d, a;
   var v = [];
@@ -38,7 +42,7 @@ function trackballView(x, y) {
   }
   return v;
 }
-
+// track mouse for rotation of the cube
 function mouseMotion(x, y) {
   var dx, dy, dz;
 
@@ -62,7 +66,7 @@ function mouseMotion(x, y) {
   }
   render();
 }
-
+// function to start rotate the cube
 function startMotion(x, y) {
   trackingMouse = true;
   startX = x;
@@ -73,7 +77,7 @@ function startMotion(x, y) {
   lastPos = trackballView(x, y);
   trackballMove = true;
 }
-
+// function to stop rotate the cube
 function stopMotion(x, y) {
   trackingMouse = false;
   if (startX != x || startY != y) {
@@ -82,8 +86,11 @@ function stopMotion(x, y) {
     trackballMove = false;
   }
 }
+
+// function to initialize the webgl and set the canvas color according to that.
 function initColorChange(gl, canvas, color) {
   gl.viewport(0, 0, canvas.width, canvas.height);
+  // Allocating the color for the canvas
   gl.clearColor(color, color, color, 1.0);
 
   gl.enable(gl.DEPTH_TEST);
@@ -93,7 +100,7 @@ function initColorChange(gl, canvas, color) {
   //
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
-
+  // Creating the buffer for color.
   var cBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
@@ -101,7 +108,7 @@ function initColorChange(gl, canvas, color) {
   var colorLoc = gl.getAttribLocation(program, "aColor");
   gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(colorLoc);
-
+  // Creating the buffer for the positions of the points.
   var vBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
@@ -109,62 +116,65 @@ function initColorChange(gl, canvas, color) {
   var positionLoc = gl.getAttribLocation(program, "aPosition");
   gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(positionLoc);
-
+  // Initialize the matrix for rotation of the cube
   rotationMatrix = mat4();
   rotationMatrixLoc = gl.getUniformLocation(program, "uRotationMatrix");
   gl.uniformMatrix4fv(rotationMatrixLoc, true, flatten(rotationMatrix));
-
+  // Adding event listener for mouse to start rotating the cube
   canvas.addEventListener("mousedown", function (event) {
     rotateAxis = false;
     var x = (3 * event.clientX) / canvas.width - 1;
     var y = (3 * (canvas.height - event.clientY)) / canvas.height - 1;
     startMotion(x, y);
   });
-
+  // Adding event listener for mouse to stop rotating the cube
   canvas.addEventListener("mouseup", function (event) {
     rotateAxis = false;
     var x = (3 * event.clientX) / canvas.width - 1;
     var y = (3 * (canvas.height - event.clientY)) / canvas.height - 1;
     stopMotion(x, y);
   });
-
+  // Adding event listener for mouse to rotate the cube on movement
   canvas.addEventListener("mousemove", function (event) {
-    // rotateAxis=false
+    //rotateAxis=false
     var x = (3 * event.clientX) / canvas.width - 1;
     var y = (3 * (canvas.height - event.clientY)) / canvas.height - 1;
     mouseMotion(x, y);
   });
-
   render();
 }
+// Function to load at the time of loading or reloading of the html page
 window.onload = function init() {
+  // Getting all the references of the button and slider.
   canvas = document.getElementById("gl-canvas");
   var buttonColor = document.getElementById("buttonColor");
   var labelColor = document.getElementById("labelColor");
-
   var myRange = document.getElementById("myRange");
+  // Initial angle to rotate the cube
   angle = JSON.parse(myRange.value) * 0.01;
+  // Changing the value of the slider
   myRange.oninput = function () {
     angle = JSON.parse(this.value) * 0.01;
   };
   var rotateX = document.getElementById("rotateX");
   rotateX.onclick = function () {
+    // Set axis to rotate on X axis
     axis = vec3(1, 0, 0);
     rotateAxis = true;
-
     rotationMatrix = mat4();
   };
   var rotateY = document.getElementById("rotateY");
   rotateY.onclick = function () {
+    // Set axis to rotate on Y axis
     axis = vec3(0, 1, 0);
     rotateAxis = true;
     rotationMatrix = mat4();
   };
   var rotateZ = document.getElementById("rotateZ");
   rotateZ.onclick = function () {
+    // Set axis to rotate on Z axis
     axis = vec3(0.57735, 0.57735, 0.57735);
     rotateAxis = true;
-
     rotationMatrix = mat4();
   };
   var body = document.getElementById("body");
@@ -175,11 +185,13 @@ window.onload = function init() {
   if (!gl) {
     alert("WebGL 2.0 isn't available");
   }
+  // Function to change color of the canvas and the background.
   buttonColor.onclick = function () {
     changeColor = !JSON.parse(buttonColor.value);
     buttonColor.value = changeColor;
     positions = [];
     colors = [];
+    // Changing the color according to the current value of the button
     if (changeColor) {
       bgColor = 0;
       body.style.backgroundColor = "black";
@@ -189,6 +201,7 @@ window.onload = function init() {
       body.style.backgroundColor = "white";
       labelColor.style.color = "black";
     }
+    // creating the colorcube 
     colorCube(changeColor);
     initColorChange(gl, canvas, bgColor);
   };
@@ -198,6 +211,7 @@ window.onload = function init() {
   initColorChange(gl, canvas, bgColor);
 };
 
+// Creating cube using quad
 function colorCube(changeColor) {
   quad(1, 0, 3, 2, changeColor);
   quad(2, 3, 7, 6, changeColor);
@@ -239,6 +253,7 @@ function quad(a, b, c, d, changeColor) {
   var indices = [a, b, c, a, c, d];
 
   for (var i = 0; i < indices.length; ++i) {
+    // positions for the next vertices to be pushed
     positions.push(vertices[indices[i]]);
     if (changeColor) {
       // for interpolated colors use
@@ -251,7 +266,9 @@ function quad(a, b, c, d, changeColor) {
 }
 
 function render() {
+  // clearing the buffer 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  // Rotation of the cube by mouse movement
   if (trackballMove) {
     axis = normalize(axis);
     if (angle > 0.1) {
@@ -260,6 +277,7 @@ function render() {
     rotationMatrix = mult(rotationMatrix, rotate(angle, axis));
     gl.uniformMatrix4fv(rotationMatrixLoc, true, flatten(rotationMatrix));
   }
+  // For the rotation of the cube on axis by buttons
   if (rotateAxis) {
     rotationMatrix = mult(rotationMatrix, rotate(angle, axis));
     gl.uniformMatrix4fv(rotationMatrixLoc, false, flatten(rotationMatrix));
